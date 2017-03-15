e83d403ea312a8f38a93fc525d54e21e8aa158a2
Greg Heartsfield <scsibug@imap.cc>
2017-03-15 04:46:46 +0000

    Added gauge.

--- web/server.py
+++ web/server.py
@@ -7,6 +7,18 @@ conn = psycopg2.connect(pg_conn_str)
 
 
 home_query_SQL = "select to_char(date_trunc('second',sample_time at time zone 'CDT'),'YYYY-MM-DD HH24:MI:SS'), watts from today_10m_usage where meter_id=30"
+last_day_SQL = "select daily_usage from last_day_usage"
+
+class LastDayUsage:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        # Query
+        curs = conn.cursor() 
+        curs.execute(last_day_SQL)
+        csv = "Last 24 Hour Usage\n"
+        for record in curs:
+            csv+= str(float(record[0])) + "\n"
+        resp.body = csv
 
 class HomeReportResource:
     def on_get(self, req, resp):
@@ -18,5 +30,7 @@ class HomeReportResource:
         for record in curs:
             csv+= str(record[0]) +","+ str(record[1]) + "\n"
         resp.body = csv
+
 api = falcon.API()
 api.add_route('/home-report', HomeReportResource())
+api.add_route('/last-day-usage',LastDayUsage())
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,3 +1,34 @@
+var gauge = c3.generate({
+    bindto: d3.select('#gauge'),
+    data: {
+        url: '/powermon/api/last-day-usage',
+        type: 'gauge'
+    },
+    gauge: {
+      min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
+      max: 60, // 100 is default
+      units: ' KWh',
+      width: 39, // for adjusting arc thickness
+      expand: true,
+      label: {
+          format: function (value, ratio) {
+            return value;
+	  }
+      }
+    },
+    color: {
+        pattern: ['#FF0000', '#F97600', '#F6C600', '#60B044'], // the three color levels for the percentage values.
+        threshold: {
+            unit: 'KWh', // percentage is default
+            max: 60, // 100 is default
+            values: [14, 20, 50]
+        }
+    },
+    size: {
+        height: 180
+    }
+    });
+
 var chart = c3.generate({
   data: {
     url: '/powermon/api/home-report',
@@ -13,3 +44,4 @@ var chart = c3.generate({
     }
   }
 });
+
