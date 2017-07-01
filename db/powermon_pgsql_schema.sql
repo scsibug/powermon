fa26f395f7593ac37d5b16e60c9b28d8549f3268
Greg Heartsfield <scsibug@imap.cc>
2017-07-01 21:01:38 +0000

    Create new views.

--- db/powermon_pgsql_schema.sql
+++ db/powermon_pgsql_schema.sql
@@ -26,3 +26,41 @@ create table meter_readings (
 );
 
 
+;; Views
+
+CREATE VIEW sample_view AS
+ SELECT s.id AS sample_id, m.id AS meter_id, m.type AS meter_type, s.reading AS consumption, timezone('UTC'::text, s.tstamp) AS tstamp, r.name AS reader
+   FROM meters m, meter_readings s, readers r
+  WHERE m.id = s.meter AND s.reader = r.id AND (m.type = ANY (ARRAY[4, 5, 7, 8]))
+  ORDER BY timezone('UTC'::text, s.tstamp) DESC;
+
+CREATE VIEW last_month_usage AS
+ SELECT round((max(sample_view.consumption) - min(sample_view.consumption))::numeric / 100.0, 1) AS daily_usage
+   FROM sample_view
+  WHERE sample_view.meter_id = 30 AND sample_view.tstamp > (now() - '720:00:00'::interval);
+
+CREATE VIEW lifetime_mean_wattage AS
+ SELECT a.meter_id, round(avg(a.watts)::numeric, 1) AS mean_watts
+   FROM ( SELECT s.meter_id, s.end_time, s.watt_hours::double precision / s.hours AS watts
+           FROM ( SELECT sample_view.meter_id, max(sample_view.tstamp) AS end_time, 10 * (max(sample_view.consumption) - min(sample_view.consumption)) AS watt_hours, date_part('epoch'::text, max(sample_view.tstamp) - min(sample_view.tstamp)) / 3600::double precision AS hours, round(date_part('epoch'::text, sample_view.tstamp) / (60 * 60)::double precision) AS rounded_tstamp
+                   FROM sample_view
+                  GROUP BY round(date_part('epoch'::text, sample_view.tstamp) / (60 * 60)::double precision), sample_view.meter_id) s
+          WHERE s.hours > 0::double precision
+          ORDER BY s.meter_id, s.end_time DESC) a
+  GROUP BY a.meter_id
+  ORDER BY round(avg(a.watts)::numeric, 1);
+
+
+CREATE VIEW today_10m_usage AS
+ SELECT counts.meter_id, counts.sample_count, counts.sample_time, round((counts.watt_hours::double precision / counts.hours)::numeric, 1) AS watts
+   FROM ( SELECT sample_view.meter_id, min(sample_view.tstamp) + (max(sample_view.tstamp) - min(sample_view.tstamp)) AS sample_time, count(*) AS sample_count, 10 * (max(sample_view.consumption) - min(sample_view.consumption)) AS watt_hours, date_part('epoch'::text, max(sample_view.tstamp) - min(sample_view.tstamp)) / 3600::double precision AS hours, round(date_part('epoch'::text, sample_view.tstamp) / (60 * 10)::double precision) AS rounded_tstamp
+           FROM sample_view
+          WHERE sample_view.tstamp > (now() - '24:00:00'::interval)
+          GROUP BY round(date_part('epoch'::text, sample_view.tstamp) / (60 * 10)::double precision), sample_view.meter_id
+          ORDER BY round(date_part('epoch'::text, sample_view.tstamp) / (60 * 10)::double precision) DESC) counts
+  WHERE counts.sample_count > 5 AND counts.watt_hours > 0;
+
+CREATE VIEW last_day_usage AS
+SELECT round((max(sample_view.consumption) - min(sample_view.consumption))::numeric / 100.0, 1) AS daily_usage
+   FROM sample_view
+  WHERE sample_view.meter_id = 30 AND sample_view.tstamp > (now() - '24:00:00'::interval);
