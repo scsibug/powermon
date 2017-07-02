3bb4a1a65b428e1f5a0983cebac4c20f9cf3a8cb
Greg Heartsfield <scsibug@imap.cc>
2017-07-02 21:29:07 +0000

    Add API endpoints for monthly historical data.

--- web/server.py
+++ web/server.py
@@ -11,6 +11,8 @@ home_query_SQL = "select to_char(date_trunc('second',sample_time at time zone 'C
 last_day_SQL = "select daily_usage from last_day_usage"
 my_last_month_SQL = "select monthly_usage from last_month_usage where meter_id=30"
 last_month_SQL = "select meter_id, monthly_usage from last_month_usage"
+last_month_hist_SQL = "select meter_id,  to_char(date_trunc('second',sample_time at time zone 'CDT'),'YYYY-MM-DD HH24:MI:SS'), watts from month_24h_usage"
+my_last_month_hist_SQL = "select meter_id,  to_char(date_trunc('second',sample_time at time zone 'CDT'),'YYYY-MM-DD HH24:MI:SS'), watts from month_24h_usage where meter_id=30"
 
 class LastDayUsage:
     def on_get(self, req, resp):
@@ -45,6 +47,27 @@ class LastMonthUsage:
             csv+= str(record[0]) +","+ str(float(record[1])) + "\n"
         resp.body = csv
 
+class LastMonthHistory:
+    def on_get(self, req, resp):
+        """Show the full history of the last month usage, by day"""
+        # Query
+        curs = conn.cursor()
+        curs.execute(last_month_hist_SQL)
+        csv = "Meter,sample_time,Watts/Day\n"
+        for record in curs:
+            csv+=str(record[0]) +","+str(record[1])+","+str(float(record[2])) + "\n"
+        resp.body = csv
+
+class MyLastMonthHistory:
+    def on_get(self, req, resp):
+        """Show the full history of the last month usage, by day"""
+        # Query
+        curs = conn.cursor()
+        curs.execute(my_last_month_hist_SQL)
+        csv = "sample_time,Watts\n"
+        for record in curs:
+            csv+=str(record[1]) +","+str(float(record[2])) + "\n"
+        resp.body = csv
 
 class HomeReportResource:
     def on_get(self, req, resp):
@@ -62,3 +85,5 @@ api.add_route('/home-report', HomeReportResource())
 api.add_route('/last-day-usage',LastDayUsage())
 api.add_route('/my-last-month-usage',MyLastMonthUsage())
 api.add_route('/last-month-usage',LastMonthUsage())
+api.add_route('/last-month-history',LastMonthHistory())
+api.add_route('/my-last-month-history',MyLastMonthHistory())
